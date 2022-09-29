d16235d5f1c10e9afb26ad92378ceb8d998966bd
Iurcik <gutan.iurii0@gmail.com>
2022-09-30 02:51:18 +0300

    upload pad project

---
+++ Desktop/Agent Meagerie/Broker/PayloadStorage.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using System.Collections.Concurrent;
+
+namespace Broker
+{
+    static class PayloadStorage
+    {
+        private static ConcurrentQueue<Payload> _pauloadQueue;
+
+        static PayloadStorage()
+        {
+            _pauloadQueue = new ConcurrentQueue<Payload>();
+        }
+
+        public static void Add(Payload payload)
+        {
+            _pauloadQueue.Enqueue(payload);
+        }
+
+        public static Payload GetNext()
+        {
+            Payload payload = null;
+            _pauloadQueue.TryDequeue(out payload);
+
+            return payload;
+        }
+
+        public static bool IsEmpty()
+        {
+            return _pauloadQueue.IsEmpty;
+        }
+    }
+}
---
+++ Desktop/Agent Meagerie/Publisher/PublisherSocket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Publisher
+{
+    class PublisherSocket
+    {
+        private Socket _socket;
+        public bool IsConnected;
+
+        public PublisherSocket()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Send(byte [] data)
+        {
+            try
+            {
+                _socket.Send(data);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Could not send data. {e.Message}");
+            }
+
+        }
+        public void Connect(string ipAddress, int port)
+        {
+            //varianta asincrona
+            _socket.BeginConnect(new IPEndPoint(IPAddress.Parse(ipAddress), port), ConnectedCallback, null);
+            Thread.Sleep(2000);
+        }
+
+        private void ConnectedCallback(IAsyncResult asyncResult)
+        {
+            if(_socket.Connected)
+            {
+                Console.WriteLine("Sender connected to Broker.");
+            }
+            else
+            {
+                Console.WriteLine("Error:Sender not connected to Broker.");
+            }
+            IsConnected = _socket.Connected;
+        }
+    }
+} 
